194532958a56f44b728913683ba95f1143eb46be
AmadeusGB <1069830494@qq.com>
2023-02-06 03:45:06 +0000

    frame adjustment

--- hd-wallet/src/main.rs
+++ hd-wallet/src/main.rs
@@ -27,21 +27,24 @@ fn main() {
     match args[1].as_str() {
         "generate" => println!("mnemonic: \"{}\"", tcx_primitive::generate_mnemonic()),
 
+        //————————————Substrate———————————//
         "substrate_pk_from_seed" => println!("public key from seed: {}", tcx_primitive::recover_pk_from_seed(&args[2])),
         "substrate_pk_from_mnemonic" => println!("public key from mnemonic: {}", tcx_primitive::recover_pk_from_mnemonic(&args[2])),
         "substrate_sk_drive_from_mnemonic" => println!("private key from mnemonic: {}", tcx_primitive::recover_sk_drive_from_mnemoic(&args[2], &args[3])),
         "substrate_ss58" => println!("ss58 address from mnemonic: {}", tcx_primitive::recover_address_from_mnemonic(&args[2])),
         "substrate_pk_drive_from_mnemonic" => println!("public key from mnemonic: {}", tcx_primitive::recover_pk_drive_from_mnemoic(&args[2], &args[3])),
-
         "export_from_secret_key" => println!("export_from_secret_key: {:?}", tcx_substrate::export_from_secret_key()),
-        //————————Substrate———————————
-
+        
+        //————————————BCH———————————//
         "bip39_mnemonic_convert_seed"  => println!("bip39 mnemonic convert seed: {:?} ", tcx_primitive::mnemonic_convert_seed(&args[2])),
         "bch_address_from_pub_key" => println!("bch_address from pub key: {}", tcx_bch::bch_address_from_pub_key(&args[2])),
         "bch_address_from_pri_key" => println!("bch_address from pri key: {}", tcx_bch::bch_address_from_pri_key(&args[2])),
         "bch_sign_tx" => println!("bch sign transaction: {}", tcx_bch::bch_sign_to_tx(&args[2], args[3].parse::<i64>().unwrap(), &args[4])),
         "bch_account_create" => println!("keystore info: {}", tcx_bch::bch_account_create(&args[2])),
         "bch_account_recover" => println!("keystore info: {}", tcx_bch::bch_account_recover(&args[2], &args[3])),
+
+        //————————————BTC———————————//
+        
 
         _ => println!("bash:command not found!"),
     }
