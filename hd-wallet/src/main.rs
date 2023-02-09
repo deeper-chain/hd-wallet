4294be5500a22f9c19d5f85ea98643fa0a81ce94
jerry-yu <anhuitianyu@163.com>
2023-02-09 15:29:25 +0800

    new main function

--- hd-wallet/src/main.rs
+++ hd-wallet/src/main.rs
@@ -17,6 +17,8 @@ use tcx_substrate::{self, SubstrateRawTxIn, SubstrateTxOut};
 // use tcx_tron;
 // use tcx_tezos;
 // use tcx_filecoin;
+mod main2;
+mod main3;
 
 pub fn encode_message(msg: impl Message) -> Vec<u8> {
     let mut buf = BytesMut::with_capacity(msg.encoded_len());
@@ -31,71 +33,39 @@ fn _to_c_char(str: &str) -> *const c_char {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    match args[1].as_str() {
-        "generate" => println!("mnemonic: \"{}\"", tcx_primitive::generate_mnemonic()),
+    main2::func(args.clone());
+    main3::func(args);
 
-        //————————————Substrate———————————//
-        "substrate_pk_from_seed" => println!(
-            "public key from seed: {}",
-            tcx_primitive::recover_pk_from_seed(&args[2])
-        ),
-        "substrate_pk_from_mnemonic" => println!(
-            "public key from mnemonic: {}",
-            tcx_primitive::recover_pk_from_mnemonic(&args[2])
-        ),
-        "substrate_sk_drive_from_mnemonic" => println!(
-            "private key from mnemonic: {}",
-            tcx_primitive::recover_sk_drive_from_mnemoic(&args[2], &args[3])
-        ),
-        "substrate_ss58" => println!(
-            "ss58 address from mnemonic: {}",
-            tcx_primitive::recover_address_from_mnemonic(&args[2])
-        ),
-        "substrate_pk_drive_from_mnemonic" => println!(
-            "public key from mnemonic: {}",
-            tcx_primitive::recover_pk_drive_from_mnemoic(&args[2], &args[3])
-        ),
-        "export_from_secret_key" => println!(
-            "export_from_secret_key: {:?}",
-            tcx_substrate::export_from_secret_key()
-        ),
+    // match args[1].as_str() {
+    //     "generate" => println!("mnemonic: \"{}\"", tcx_primitive::generate_mnemonic()),
 
-        //————————————BCH———————————//
-        "bip39_mnemonic_convert_seed" => println!(
-            "bip39 mnemonic convert seed: {:?} ",
-            tcx_primitive::mnemonic_convert_seed(&args[2])
-        ),
-        "bch_address_from_pub_key" => println!(
-            "bch_address from pub key: {}",
-            tcx_bch::bch_address_from_pub_key(&args[2])
-        ),
-        "bch_address_from_pri_key" => println!(
-            "bch_address from pri key: {}",
-            tcx_bch::bch_address_from_pri_key(&args[2])
-        ),
-        "bch_sign_tx" => println!(
-            "bch sign transaction: {}",
-            tcx_bch::bch_sign_to_tx(&args[2], args[3].parse::<i64>().unwrap(), &args[4])
-        ),
-        "bch_account_create" => {
-            println!("keystore info: {}", tcx_bch::bch_account_create(&args[2]))
-        }
-        "bch_account_recover" => println!(
-            "keystore info: {}",
-            tcx_bch::bch_account_recover(&args[2], &args[3])
-        ),
+    //     //————————————Substrate———————————//
+    //     "substrate_pk_from_seed" => println!("public key from seed: {}", tcx_primitive::recover_pk_from_seed(&args[2])),
+    //     "substrate_pk_from_mnemonic" => println!("public key from mnemonic: {}", tcx_primitive::recover_pk_from_mnemonic(&args[2])),
+    //     "substrate_sk_drive_from_mnemonic" => println!("private key from mnemonic: {}", tcx_primitive::recover_sk_drive_from_mnemoic(&args[2], &args[3])),
+    //     "substrate_ss58" => println!("ss58 address from mnemonic: {}", tcx_primitive::recover_address_from_mnemonic(&args[2])),
+    //     "substrate_pk_drive_from_mnemonic" => println!("public key from mnemonic: {}", tcx_primitive::recover_pk_drive_from_mnemoic(&args[2], &args[3])),
+    //     "export_from_secret_key" => println!("export_from_secret_key: {:?}", tcx_substrate::export_from_secret_key()),
+        
+    //     //————————————BCH———————————//
+    //     "bip39_mnemonic_convert_seed"  => println!("bip39 mnemonic convert seed: {:?} ", tcx_primitive::mnemonic_convert_seed(&args[2])),
+    //     "bch_address_from_pub_key" => println!("bch_address from pub key: {}", tcx_bch::bch_address_from_pub_key(&args[2])),
+    //     "bch_address_from_pri_key" => println!("bch_address from pri key: {}", tcx_bch::bch_address_from_pri_key(&args[2])),
+    //     "bch_sign_tx" => println!("bch sign transaction: {}", tcx_bch::bch_sign_to_tx(&args[2], args[3].parse::<i64>().unwrap(), &args[4])),
+    //     "bch_account_create" => println!("keystore info: {}", tcx_bch::bch_account_create(&args[2])),
+    //     "bch_account_recover" => println!("keystore info: {}", tcx_bch::bch_account_recover(&args[2], &args[3])),
 
-        //————————————BTC———————————//
-        _ => main2(&args[1], &args[2]),
-    }
+    //     //————————————BTC———————————//
+    //     _ => main2::func(&args[1], &args[2]),
+    // }
 }
 
 fn main2(command: &str, content: &str) {
     match command {
         // api for substrate
         "hd_import" => hd_import(content),
-        "hd_working" => hd_working(),
-
+        "hd_working" => hd_working(),       
+        
         _ => println!("bash:command not found!"),
     }
 }
---
+++ hd-wallet/src/main3.rs
@@ -0,0 +1,5 @@
+
+
+pub fn func(args: Vec<String>) {
+
+}
